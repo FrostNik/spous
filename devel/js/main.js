c121b158b7e5d878e2556ba280a8215d54d0d3fe
FrostNik <tytarenko.dm@gmail.com>
2021-02-24 12:01:43 +0200

    chat

--- devel/js/main.js
+++ devel/js/main.js
@@ -10,4 +10,35 @@ $(function () {
       $('.dropdown-content').slideUp(200);
     }
   });
+  
+  $('.toggle-btn').on('click', function () {
+    $(this).toggleClass('active');
+  });
+
+var customSelectWrap       = '.custom-select__wrap',
+    customSelectCurrent    = '.custom-select__current',
+    customSelectDrop       = '.custom-select__drop',
+    customSelectOption     = '.option';
+$(customSelectCurrent).each(function() {
+  var value = $(this).next(customSelectDrop).find(customSelectOption + ':nth-child(2)').html();
+  $(this).html(value);
+});
+$(customSelectCurrent).on('click', function() {
+  $(customSelectDrop).slideUp();
+  $(customSelectCurrent).not(this).removeClass('open');
+  $(this).toggleClass('open').next(customSelectDrop).stop().slideToggle();
+});
+$(customSelectOption).on('click', function() {
+  var value = $(this).html();
+  $(this).closest(customSelectWrap).find(customSelectCurrent).removeClass('open').html(value)
+    .next(customSelectDrop).slideUp();
+});
+$(document).mouseup(function (e) {
+    var container = $(customSelectWrap);
+    if (e.target!=container[0] && !container.has(e.target).length){
+        container.find(customSelectCurrent).removeClass('open');
+        container.find(customSelectDrop).slideUp();
+    }
+});
+
 })
